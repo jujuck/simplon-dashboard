d6adc0f989e0490db6b2de84b811ff232b2268f6
Julien Richard <julien.y.richard@proton.me>
2026-07-03 09:18:25 +0200

    feat: ajout de lien vers les comptes rendu de réunion

--- script.js
+++ script.js
@@ -164,6 +164,18 @@ const dashboardData = {
           name: "Pedago Parcours infra",
           url: "https://drive.google.com/drive/folders/0AGOHaDL1McfhUk9PVA",
         },
+        {
+          name: "Synchro Cloé",
+          url: "https://docs.google.com/document/d/1GMpU2YgjOmd7oAC6GPrCDv9UL6-eaqMlc6HdY7iWoq0/edit?tab",
+        },
+        {
+          name: "Synchro Cloé",
+          url: "https://docs.google.com/document/d/1GMpU2YgjOmd7oAC6GPrCDv9UL6-eaqMlc6HdY7iWoq0/edit?tab",
+        },
+        {
+          name: "Réunion équipe distanciel",
+          url: "https://docs.google.com/document/d/1ogHa5fArUJ9mket7SDV3iDLyHA4AO5aJVdbuFA1GHsg/edit?tab=t.ax0fmx9xsygh#heading=h.fkg4xesc0wls",
+        },
       ],
     },
     {
